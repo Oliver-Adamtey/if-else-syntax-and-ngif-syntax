10c4df7d5bed7feb91358393da272b2d1f8a88d6
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-01-12 15:38:12 +0000

    Create angular-course-card.component.ts

---
+++ src/app/angular-course-card/angular-course-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-course-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './angular-course-card.component.html',
+  styleUrl: './angular-course-card.component.scss'
+})
+export class AngularCourseCardComponent {
+
+}
