8aa5f09ac0857416381436489a767bc95a9e5e78
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-01-12 15:48:12 +0000

    Create db-data.ts

---
+++ src/app/angular-course-card/db-data.ts
@@ -0,0 +1,34 @@
+export const COURSE: any = [
+    {
+        id: 1,
+        description: "Angular Core Deep Dive",
+        iconUrl: 'https://blog.angular-university.io/angular-if/',
+        longDescription: "A detailed walk-through of the most important part of Angular",
+        lessonsCount: 10,
+        category: 'INTERMEDIATE'
+    },
+    {
+        id: 1,
+        description: "Angular Core Deep Dive",
+        iconUrl: 'https://blog.angular-university.io/angular-if/',
+        longDescription: "A detailed walk-through of the most important part of Angular",
+        lessonsCount: 10,
+        category: 'INTERMEDIATE'
+    },
+    {
+        id: 1,
+        description: "RxJs Core Deep Dive",
+        iconUrl: 'https://blog.angular-university.io/angular-if/',
+        longDescription: "A detailed walk-through of the most important part of Angular",
+        lessonsCount: 10,
+        category: 'ADVANCE'
+    },
+    {
+        id: 1,
+        description: "NgRx Core Deep Dive",
+        iconUrl: 'https://blog.angular-university.io/angular-if/',
+        longDescription: "A detailed walk-through of the most important part of Angular",
+        lessonsCount: 10,
+        category: 'BEGINNER'
+    }
+]
