cd18f22902aecd6ea00e6de181a2f0359728b75b
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-01-12 15:40:16 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AngularCourseCardComponent } from './angular-course-card/angular-course-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AngularCourseCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
